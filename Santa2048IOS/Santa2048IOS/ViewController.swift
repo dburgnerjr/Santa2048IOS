aad5db7fc5983a64b716a723d4ff63478774aad9
Daniel Burgner <dburgnerjr@yahoo.com>
2017-11-12 21:20:33 -0600

    Project init.
    Initialized project in Xcode.

---
+++ Santa2048IOS/Santa2048IOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Santa2048IOS
+//
+//  Created by Daniel Burgner on 11/12/17.
+//  Copyright © 2017 Daniel Burgner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
