2729f7c15f99709e70f0f01d6c0633897a811187
Daniel Burgner <dburgnerjr@yahoo.com>
2017-11-18 15:35:15 -0600

    Accessory file.
    Accessory file finished per tutorial
    https://www.youtube.com/watch?v=snBBeOVhuZQ

--- Santa2048IOS/Santa2048IOS/Views/Accessory.swift
+++ Santa2048IOS/Santa2048IOS/Views/Accessory.swift
@@ -6,4 +6,40 @@
 //  Copyright © 2017 Daniel Burgner. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol ScoreViewProtocol {
+    func scoreChanged(newScore s: Int)
+}
+
+class ScoreView : UIView, ScoreViewProtocol {
+    var score: Int = 0 {
+        didSet {
+            label.text = "SCORE: \(score)"
+        }
+    }
+    let defaultFrame = CGRect(origin: .zero, size: CGSize(width: 140, height: 140))
+    var label: UILabel
+    
+    init(backgroundColoe bgcolor: UIColor, textColor tcolor:
+        UIColor, font: UIFont, radius r: CGFloat) {
+        label = UILabel(frame: defaultFrame)
+        label.textAlignment = NSTextAlignment.center
+        super.init(frame: defaultFrame)
+        
+        backgroundColor = bgcolor
+        label.textColor = tcolor
+        label.font = font
+        
+        layer.cornerRadius = r
+        self.addSubview(label)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("NSCoding not supported")
+    }
+    
+    func scoreChanged(newScore s: Int) {
+        let defaultFrame = CGRect(origin: .zero, size: CGSize(width: 140, height: 140))
+    }
+}
