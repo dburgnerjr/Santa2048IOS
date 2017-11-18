eb3c48a3db0d26379cbc30257dc3635db63fc143
Daniel Burgner <dburgnerjr@yahoo.com>
2017-11-18 16:20:34 -0600

    GameModel (part 1)
    GameModel setup part 1 per tutorial
    https://www.youtube.com/watch?v=eCGBHuMOW44

--- Santa2048IOS/Santa2048IOS/Models/GameModel.swift
+++ Santa2048IOS/Santa2048IOS/Models/GameModel.swift
@@ -6,4 +6,82 @@
 //  Copyright © 2017 Daniel Burgner. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol GameModelProtocol : class {
+    func scoreChange(score: Int)
+    func moveOneTile(from: (Int, Int), to: (Int, Int), value: Int)
+    func moveTwoTile(from: ((Int, Int), (Int, Int)), to: (Int, Int), value: Int)
+    func insertTile(location: (Int, Int), value: Int)
+}
+
+class GameModel: NSObject {
+    let dimension: Int
+    let threshold: Int
+    
+    var score: Int = 0 {
+        didSet {
+            delegate.scoreChanged(score)
+        }
+    }
+    var gameboard: SquareGameboard<TileObject>
+    
+    let delegate: GameModelProtocol
+    
+    var queue: [MoveCommand]
+    var timer: NSTimer
+
+    let maxCommands = 100
+    let queueDelay = 0.3
+    
+    init(dimension d: Int, threshold t: Int, delegate: GameModelProtocol) {
+        dimension = d
+        threshold = t
+        self.delegate = delegate
+        queue = [MoveCommand]()
+        timer = NSTimer()
+        gameboard = SquareGameboard(dimension: d, initialValue: .Empty)
+        super.init()
+    }
+    
+    func reset() {
+        score = 0
+        gameboard.setAll(.Empty)
+        queue.removeAll(keepCapacity: true)
+        timer.invalidate()
+    }
+    
+    func queueMove(direction: MoveDirection, completion: (Bool) -> ()) {
+        if queue.count > maxCommands {
+            return
+        }
+
+        let command = MoveCommand(d: direction, c: completion)
+        queue.append(command)
+        if (!timer.valid) {
+            timerFired(timer)
+        }
+    }
+    
+    func timerFired(timer: NSTimer) {
+        if queue.count == 0 {
+            return
+        }
+        
+        var changed = false
+        while queue.count > 0 {
+            let command = queue[0]
+            queue.removeAtIndex(0)
+            changed = preformMove(command.direction)
+            command.completion(changed)
+            if (changed) {
+                break
+            }
+            if changed {
+                self.timer = NSTimer.scheduledTimerWithTimeInterval
+                (queueDelay, target: self, selector: Selector("timerFired:"),
+                 userInfo: nil, repeats: false)
+            }
+        }
+    }
+}
